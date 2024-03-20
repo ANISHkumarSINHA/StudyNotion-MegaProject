d80740988b8ddc8c2b7b58eb2f560f922b3e1d33
Anish Sinha <112953617+ANISHkumarSINHA@users.noreply.github.com>
2024-03-20 19:34:09 +0530

    Update Section.js

--- server/controllers/Section.js
+++ server/controllers/Section.js
@@ -1,7 +1,7 @@
 const Section = require("../models/Section")
 const Course = require("../models/Course")
 const SubSection = require("../models/Subsection")
-// CREATE a new section
+ // CREATE a new section
 exports.createSection = async (req, res) => {
   try {
     // Extract the required properties from the request body
